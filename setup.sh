f8dff885fcbb84325e555369812f73096b2b833a
Alexej Disterhoft <github@disterhoft.de>
2022-12-23 21:25:16 +0100

    reproduce issue

--- setup.sh
+++ setup.sh
@@ -13,6 +13,15 @@ main() {
   git add . && git commit -m "bump: version v0.1.2"
   git tag v0.1.2 -m "v0.1.2"
 
+  : commit a breaking change and tag a new prerelease
+  touch file0.txt
+  git add file0.txt && git commit -F - <<< """
+feat: test
+
+BREAKING CHANGE: test
+"""
+  python3 -m semantic_release -v DEBUG version --prerelease
+
   : replicate git history from PR
   for i in "1" "2" "3"; do
     touch "file$i.txt"
